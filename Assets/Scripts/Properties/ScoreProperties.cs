3f06179723b02c7eafcf22001e523fdf019554f4
Pavel Sinitsky <adronex303@gmail.com>
2015-09-28 13:38:28 +0300

    .gitignore issues

--- Assets/Scripts/Properties/ScoreProperties.cs
+++
@@ -1,5 +0,0 @@
-public static class ScoreProperties {
-	
-	public const string SCREEN_NAME_LEVEL_COMPLETE = "LevelCompleteScreen";
-	
-}
--- Assets/Scripts/Properties/Tags.cs
+++
@@ -1,6 +0,0 @@
-public static class Tags {
-
-	public const string BALL = "Ball";
-	public const string BASKET = "Basket";
-	public const string STICKY_PLATFORM = "StickyPlatform";
-}

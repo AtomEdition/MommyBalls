1a0dfc3d23c53069ca71b092329fb33cf5445e81
Pavel Sinitsky <adronex303@gmail.com>
2015-10-22 11:07:13 +0300

    Backgrounds of first locations and space created

---
+++ Assets/Scripts/Controllers/Background/CloudController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	private const float OUT_OF_LEVEL_RANGE = 8F;	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CheckCloudPosition ();
+	}
+
+	private void CheckCloudPosition() {
+		if (transform.position.x > OUT_OF_LEVEL_RANGE
+			|| transform.position.x < -OUT_OF_LEVEL_RANGE) {
+
+			Vector2 oldVector = gameObject.transform.position;
+			Vector2 newVector = new Vector2 (-oldVector.x, oldVector.y);
+			transform.position = newVector;
+		}
+	}
+}

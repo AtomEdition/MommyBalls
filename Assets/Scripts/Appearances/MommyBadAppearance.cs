3c49a75442b449db325c78e0c5f6b92f4271140d
Pavel Sinitsky <adronex303@gmail.com>
2015-11-04 11:54:47 +0300

    Jumpers reworked

---
+++ Assets/Scripts/Appearances/MommyBadAppearance.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MommyBadAppearance : Appearance {
+	
+	private const string IDLE_ANIMATION_TAG = "Idle";
+	private const string DIE_ANIMATION_TAG = "Happy";
+	private const string CATCH_ANIMATION_TAG = "Sad";
+	private const float APPEARANCE_SIZE_MULTIPLIER = 0.8F;
+
+	public GameObject animationPrefab;
+
+	LevelService levelService = Singleton<LevelService>.GetInstance();
+
+	// Use this for initialization
+	void Start () {
+		AttachAnimation ();
+		levelService.OnBallCreate.eventAttachTo += SetIdleAnimation;
+		levelService.OnBallCatch.eventAttachTo += SetCatchedAnimation;
+		levelService.OnBallDestroy.eventAttachTo += SetDieAnimation;
+	}	
+
+	private void AttachAnimation() {
+
+		GameObject newBallAppearance = Instantiate (animationPrefab) as GameObject;
+
+		Vector3 vec = gameObject.transform.localScale;
+		newBallAppearance.transform.localScale = new Vector3(vec.x, vec.y) * APPEARANCE_SIZE_MULTIPLIER;
+
+		newBallAppearance.transform.position = gameObject.transform.position;
+		newBallAppearance.transform.parent = gameObject.transform;
+	}
+		
+	private void SetIdleAnimation() {
+
+		this.GetComponentInChildren<Animator> ().SetTrigger (IDLE_ANIMATION_TAG);
+	}
+
+	private void SetDieAnimation() {
+
+		this.GetComponentInChildren<Animator> ().SetTrigger (DIE_ANIMATION_TAG);
+	}
+
+	private void SetCatchedAnimation() {
+
+		this.GetComponentInChildren<Animator> ().SetTrigger (CATCH_ANIMATION_TAG);
+	}
+}

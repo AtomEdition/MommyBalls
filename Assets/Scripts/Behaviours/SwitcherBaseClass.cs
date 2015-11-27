20e1d227a5618a295cccb1df2593ad6612c8e529
Pavel Sinitsky <adronex303@gmail.com>
2015-11-27 15:34:00 +0300

    #37, switchers reworked, delay activator particles fix.

---
+++ Assets/Scripts/Behaviours/SwitcherBaseClass.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitcherBaseClass : MonoBehaviour {
+
+	protected void SwitchAllComponents(GameObject obj) {
+		
+		SwitchRenderers<SpriteRenderer>(obj);
+		SwitchBehaviours<BoxCollider2D>(obj);
+		SwitchBehaviours<CircleCollider2D>(obj);
+		SwitchParticles(obj);
+	}
+	
+	private void SwitchBehaviours<T>(GameObject obj) where T : Behaviour {
+		
+		foreach (T t in obj.GetComponentsInChildren<T>()) {
+			
+			if (t != null) {t.enabled = !t.enabled;}
+		}
+	}
+	
+	private void SwitchRenderers<T>(GameObject obj) where T : Renderer {
+		
+		foreach (T t in obj.GetComponentsInChildren<T>()) {
+			
+			if (t != null) {t.enabled = !t.enabled;}
+		}
+	}
+	
+	private void SwitchParticles(GameObject obj) {
+		
+		foreach (ParticleSystem t in obj.GetComponentsInChildren<ParticleSystem>()) {
+			
+			if (t != null && t.isPlaying) {
+				t.Stop ();
+			} else {
+				t.Play ();
+			}
+		}
+	}
+}
--- Assets/Scripts/Controllers/Game/DelayActivatorController.cs
+++ Assets/Scripts/Controllers/Game/DelayActivatorController.cs
@@ -1,20 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DelayActivatorController : MonoBehaviour {
+public class DelayActivatorController : SwitcherBaseClass {
 
 	public float secondsOfDelay;
 	public GameObject[] elementsToActivateDelay = {};
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 		
 	IEnumerator OnTriggerEnter2D(Collider2D trigger){
 		
@@ -32,22 +22,9 @@ public class DelayActivatorController : MonoBehaviour {
 					
 		foreach (GameObject obj in elementsToActivateDelay) {
 			
-			if (obj.GetComponent<Renderer>() != null) {
-				
-				obj.GetComponent<Renderer>().enabled = !obj.GetComponent<Renderer>().enabled;
-			}
-			if (obj.GetComponent<BoxCollider2D>() != null) {
-				
-				obj.GetComponent<BoxCollider2D>().enabled = !obj.GetComponent<BoxCollider2D>().enabled;
-			}
-			if (obj.GetComponent<CircleCollider2D>() != null) {
-				
-				obj.GetComponent<CircleCollider2D>().enabled = !obj.GetComponent<CircleCollider2D>().enabled;
-			}
-
+			SwitchAllComponents(obj);
 		}
 
-		this.gameObject.GetComponent<Renderer>().enabled = !this.gameObject.GetComponent<Renderer>().enabled;
-		this.gameObject.GetComponent<Collider2D>().enabled = !this.gameObject.GetComponent<Collider2D>().enabled;
+		SwitchAllComponents (gameObject);
 	}
 }

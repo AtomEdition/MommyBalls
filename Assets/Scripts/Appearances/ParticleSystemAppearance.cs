3f2c5c55b8f6a585d4d4030d4105d41abb3f9968
Pavel Sinitsky <adronex303@gmail.com>
2015-11-27 20:57:04 +0300

    #39, added particles on promotion button.

--- Assets/Scripts/Appearances/ParticleSystemAppearance.cs
+++ Assets/Scripts/Appearances/ParticleSystemAppearance.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BallDieParticleSystem : MonoBehaviour {
+public class ParticleSystemAppearance : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
